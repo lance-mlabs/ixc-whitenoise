9e3f7ca677957f6e758effcb047c08b9dfecbf77
Alastair Weakley <alastair@interaction.net.au>
2017-12-14 14:21:41 +1100

    avoid early import of models in django 1.10

--- ixc_whitenoise/__init__.py
+++ ixc_whitenoise/__init__.py
@@ -1,6 +1,2 @@
 import ixc_whitenoise.monkeypatch_django16
 
-from ixc_whitenoise.middleware import \
-    StripVaryHeaderMiddleware, WhiteNoiseMiddleware
-from ixc_whitenoise.storage import \
-    CompressedManifestStaticFilesStorage, UniqueStorage
