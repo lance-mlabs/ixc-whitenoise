c0adac3c60f30715185e4f060e223660bfa2a984
joaquinpunales1992 <joaquinpunales@gmail.com>
2021-10-21 12:33:23 -0500

    increase uniquefile length to allow longer file names in filefields, specifically for agsa

--- ixc_whitenoise/models.py
+++ ixc_whitenoise/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class UniqueFile(models.Model):
     created = models.DateTimeField(auto_now_add=True)
-    name = models.CharField(db_index=True, max_length=255)
-    original_name = models.CharField(db_index=True, max_length=255)
+    name = models.CharField(db_index=True, max_length=500)
+    original_name = models.CharField(db_index=True, max_length=500)
 
     class Meta:
         ordering = ('-pk', )
